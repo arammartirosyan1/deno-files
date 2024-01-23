8739e5a91a597066250135d8ab11844caad9b340
arammartirosyan1 <arammartirosyan788@gmail.com>
2024-01-23 18:32:02 +0400

    Five

--- server.ts
+++ server.ts
@@ -1,20 +1,8 @@
 import { Router } from "./shared/dependencies.ts";
 import { Application } from "./shared/dependencies.ts";
-import {
-  viewEngine,
-  engineFactory,
-  adapterFactory
-} from 'https://deno.land/x/view_engine@v1.3.0/mod.ts';
-
 
 
 const app = new Application();
-// app.use(idMiddleware);
-const ejsEngine = engineFactory.getEjsEngine();
-const oakAdapter = adapterFactory.getOakAdapter();
-
-app.use(viewEngine(oakAdapter, ejsEngine));
-
 
 const mainRouter = new Router();
 
