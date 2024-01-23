b842c4808e31a59e952fdf16ee92fbd66a7f215e
arammartirosyan1 <arammartirosyan788@gmail.com>
2024-01-23 18:38:52 +0400

    six

--- server.ts
+++ server.ts
@@ -1,15 +1,22 @@
 import { Router } from "./shared/dependencies.ts";
 import { Application } from "./shared/dependencies.ts";
+import { render } from "https://deno.land/x/dejs@0.10.3/mod.ts";
 
 
 const app = new Application();
-
 const mainRouter = new Router();
 
 mainRouter
-    .get("/", (ctx) => {
-        const test = fetch("./public/download/index.html")
-        ctx.response.body = test
+    .get("/", async (ctx) => {
+        const template = `<body>
+  <% if (name) { %>
+    <h1>hello, <%= name %>!</h1>
+  <% } %>
+</body>`;
+
+        ctx.response.body = await render(template, {
+            name: "world",
+          });
     })
     .get("/:id",(ctx) => {
         ctx.response.body = "id page"
